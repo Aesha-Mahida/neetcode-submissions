5a736ecab9895d070d0456bad4ea0a0cef6174dd
Aesha-Mahida <88031041+Aesha-Mahida@users.noreply.github.com>
2026-07-28 12:58:51 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.ts
@@ -0,0 +1,41 @@
+function isAlphaNumeric(char: string): boolean {
+    if (!char) return false;
+
+    const code = char.charCodeAt(0);
+    return (
+        (code >= 48 && code <= 57) ||  // 0-9
+        (code >= 65 && code <= 90) ||  // A-Z
+        (code >= 97 && code <= 122)    // a-z
+    );
+};
+
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    
+
+    isPalindrome(s: string): boolean {
+        let left = 0;
+        let right = s.length - 1;
+
+        while(left < right){
+            while(left < right && !isAlphaNumeric(s[left])){
+                left++;
+            }
+            while(left < right && !isAlphaNumeric(s[right])){
+                right--;
+            }
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            }
+
+            left++;
+            right--;
+        };
+
+        return true;
+    }
+}
